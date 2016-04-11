68e3b8efc26f1bd70a7ef0a0be98ca56f17131a1
Tyager <teyager@gmail.com>
2016-04-11 19:22:29 -0400

    Unbreaking the branch

--- client/components/shell/shell.controller.js
+++ client/components/shell/shell.controller.js
@@ -23,41 +23,35 @@ angular.module('musicManApp').controller('ShellCtrl', function($mdSidenav, $mdDi
         originatorEv = ev;
         $mdOpenMenu(ev);
     };
-    
-    $scope.newNotifications = [
-        {
-            id: 1234,
-            title: 'someone did something'                
-        }, {
-            id: 456,
-            title: 'someone did something else'
-        }, {
-            id: 839,
-            title: 'new upload'
-        }, {
-            id: 9742,
-            title: 'ABCD forked your song!'
-        }
-    ]; // TODO: get from server
-        
-    $scope.showUpload = function (ev) {
+
+    $scope.newNotifications = [{
+        id: 1234,
+        title: 'someone did something'
+    }, {
+        id: 456,
+        title: 'someone did something else'
+    }, {
+        id: 839,
+        title: 'new upload'
+    }, {
+        id: 9742,
+        title: 'ABCD forked your song!'
+    }]; // TODO: get from server
+
+    $scope.showUpload = function(ev) {
         // TODO: implement upload dialog
         console.log('Upload clicked');
-    };    
-
-    $scope.notificationsEnabled = true;
-    $scope.toggleNotifications = function() {
-        $scope.notificationsEnabled = !$scope.notificationsEnabled;
     };
+
     $scope.libraries = [
         { name: 'Guitars' },
         { name: 'Drums' },
         { name: 'Funky Bass' },
         { name: 'Female Vocals' },
-        { name: 'Sludgy Jazz'},
+        { name: 'Sludgy Jazz' },
         { name: '70\'s Samples' }
     ]; // TODO: get from server
-    
+
     $scope.collaborations = [
         { name: 'Ballad of Otis Grimsby' },
         { name: 'Jazzy J\'s Funky Flex' },
@@ -94,14 +88,6 @@ angular.module('musicManApp').controller('ShellCtrl', function($mdSidenav, $mdDi
         });
     };
 
-    // $scope.uploadFile = function() {
-    //     var file = $scope.myFile;
-    //     console.log('file is ');
-    //     console.dir(file);
-
-    //     $http.post('/api/things', { fileName: $scope.myFile.name, fileType: $scope.myFile.type, fileLength: $scope.myFile.size });
-    // };
-
     $scope.setFiles = function(element) {
         $scope.$apply(function(scope) {
             console.log('files:', element.files);
@@ -110,51 +96,14 @@ angular.module('musicManApp').controller('ShellCtrl', function($mdSidenav, $mdDi
             for (var i = 0; i < element.files.length; i++) {
                 scope.files.push(element.files[i])
             }
-            scope.progressVisible = false
         });
     };
 
     $scope.uploadFile = function() {
         var fd = new FormData()
         for (var i in $scope.files) {
-             $http.post('/api/things', { fileName: $scope.files[i].name, fileType: $scope.files[i].type, fileLength: $scope.files[i].size });
-
-            fd.append("uploadedFile", $scope.files[i])
+            $http.post('/api/things', { fileName: $scope.files[i].name, fileType: $scope.files[i].type, fileLength: $scope.files[i].size });
         }
-        var xhr = new XMLHttpRequest()
-        xhr.upload.addEventListener("progress", uploadProgress, false)
-        xhr.addEventListener("load", uploadComplete, false)
-        xhr.addEventListener("error", uploadFailed, false)
-        xhr.addEventListener("abort", uploadCanceled, false)
-        xhr.open("POST", "/fileupload")
-        $scope.progressVisible = true
-        // xhr.send(fd)
-    }
-
-    function uploadProgress(evt) {
-        $scope.$apply(function() {
-            if (evt.lengthComputable) {
-                $scope.progress = Math.round(evt.loaded * 100 / evt.total)
-            } else {
-                $scope.progress = 'unable to compute'
-            }
-        })
-    }
-
-    function uploadComplete(evt) {
-        /* This event is raised when the server send back a response */
-        alert(evt.target.responseText)
-    }
-
-    function uploadFailed(evt) {
-        alert("There was an error attempting to upload the file.")
-    }
-
-    function uploadCanceled(evt) {
-        $scope.$apply(function() {
-            $scope.progressVisible = false
-        })
-        alert("The upload has been canceled by the user or the browser dropped the connection.")
     }
 });
 
